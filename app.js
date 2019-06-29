f821aef14bd91c1e70b0db64116a1e9962552342
Ribjare <iceptalves@gmail.com>
2019-06-30 00:29:29 +0100

    Falta testar os ultimos
    -team -torneio

--- app.js
+++ app.js
@@ -10,48 +10,57 @@ app.use(express.static("www"));
 
 // Commands
 //to get all players
-app.get('/jogadores', requestHandlers.getJogador);
+app.get('/jogador', requestHandlers.getJogador);
 
 //to get all tornaments
-app.get('/torneios', requestHandlers.getTorneio);
+app.get('/torneio', requestHandlers.getTorneio);
 
 //to get all teams
+app.get('/equipa', requestHandlers.getEquipa);
 
 //to get all games
+app.get('/jogo', requestHandlers.getJogos);
 
 //to get games per tornament
+app.get('/torneio/:id/jogo', requestHandlers.getJogosFromTorneio);
 
 //to get teams per tornament
+app.get("/torneio/:id/equipa", requestHandlers.getEquipaFromTorneio);
 
 //to get games per team
 
-//to get all players
-
 //to get players of a team
 
-//to get
-
 //to create a player
+app.post("/jogador", requestHandlers.createUpdateJogador);
 
 //to create a team
+app.post("/equipa", requestHandlers.createUpdateEquipa);
 
 //to create a tornament
+app.post("/torneio", requestHandlers.createUpdateTorneio);
 
 //to create a game
 
 //to update a player
+app.put("/jogador/:id", requestHandlers.createUpdateJogador);
 
 //to update a team
+app.put("/equipa/:id", requestHandlers.createUpdateEquipa);
 
 //to update a tornament
+app.put("/torneio/:id");
 
 //to update a game
 
 //to delete a player
+app.delete("/jogador/:id", requestHandlers.removeJogador);
 
 //to delete a team
+app.delete("/equipa/:id", requestHandlers.deleteEquipa);
 
 //to delete a tornament
+app.delete("/torneio/:id", requestHandlers.deleteTorneio);
 
 //to delete a game
 
--- scripts/request-handlers.js
+++ scripts/request-handlers.js
@@ -36,7 +36,11 @@ function getTorneio(req, res) {
         }
     });
 }
-
+/**
+ * Função para retornar a lista de equipas da BD.
+ * @param {*} req 
+ * @param {*} res 
+ */
 function getEquipa(req, res){
     let connection = mysql.createConnection(options);
     connection.connect();
@@ -49,7 +53,11 @@ function getEquipa(req, res){
         }
     });
 }
-
+/**
+ * Função para retornar a lista de modalidades da BD.
+ * @param {*} req 
+ * @param {*} res 
+ */
 function getModalidade(req, res){
     let connection = mysql.createConnection(options);
     connection.connect();
@@ -62,7 +70,11 @@ function getModalidade(req, res){
         }
     });
 }
-
+/**
+ * Função para retornar a lista de tipos de torneio da BD.
+ * @param {*} req 
+ * @param {*} res 
+ */
 function getTipoTorneio(req, res){
     let connection = mysql.createConnection(options);
     connection.connect();
@@ -76,7 +88,11 @@ function getTipoTorneio(req, res){
     });
 }
 
-/** gets all games*/ 
+/**
+ * Função para retornar a lista de jogos da BD.
+ * @param {*} req 
+ * @param {*} res 
+ */
 function getJogos(req, res){
     let connection = mysql.createConnection(options);
     connection.connect();
@@ -89,11 +105,45 @@ function getJogos(req, res){
         }
     });
 }
+/**
+ * Função para retornar a lista de jogos de torneios da BD.
+ * @param {*} req 
+ * @param {*} res 
+ */
 function getJogosFromTorneio(req, res){
+    let idTorneio = req.params.id;
     let connection = mysql.createConnection(options);
     connection.connect();
-    let query = "SELECT id, DATE_FORMAT(birthDate, '%Y-%m-%d') as birthDate, equipa1, equipa2 FROM Jogo";
-    connection.query(query, function (err, rows) {
+    let query = "SELECT Jogo.id, e1.nome as Equipa1, e2.nome as Equipa2, torneio.name as NomeTorneio FROM Jogo ";
+    query += "join torneioequipa on Jogo.id = torneioequipa.idEquipa ";
+    query += "join Torneio on torneio.id = torneioequipa.idTorneio ";
+    query += "join equipa e1 on e1.id=jogo.equipa1 "; 
+    query += "join equipa e2 on e2.id=jogo.equipa2 where torneio.id = ? ";
+
+    connection.query(query, idTorneio, function (err, rows) {
+        if (err) {
+            res.json({ "message": "Error", "error": err });
+        } else {
+            res.json({ "message": "Success", "jogos": rows });
+        }
+    });
+}
+/**
+ * Função para retornar a lista de jogos de torneios da BD.
+ * @param {*} req 
+ * @param {*} res 
+ */
+function getEquipaFromTorneio(req, res){
+    let idTorneio = req.params.id;
+    let connection = mysql.createConnection(options);
+    connection.connect();
+    let query = "SELECT e.id, e.nome ";
+    query += "FROM equipa e ";
+    query += "join torneioequipa t on t.idEquipa = e.id ";
+    query += "join torneio on torneio.id = t.idTorneio ";
+    query += "where torneio.id = ? ";
+
+    connection.query(query, idTorneio, function (err, rows) {
         if (err) {
             res.json({ "message": "Error", "error": err });
         } else {
@@ -101,17 +151,20 @@ function getJogosFromTorneio(req, res){
         }
     });
 }
-
-function createUpdatePerson(req, res) {
-    /** @todo Completar */
+/**
+ * Creates or updates a player
+ * //maybe criar uma equipa automaticamente se nao existir???
+ */
+function createUpdateJogador(req, res){
     let connection = mysql.createConnection(options);
     let name = req.body.name;
     let birthday = req.body.birthDate;
-    let idCountry = req.body.idCountry;
-    let sql = (req.method === 'PUT') ? "UPDATE person SET name = ?, birthdate = ? , idCountry = ? WHERE id = ?" : "INSERT INTO person(name, birthdate, idCountry) VALUES (?,?,?)";
+    let nTelemovel = req.body.nTelemovel;
+    let idEquipa = req.body.idEquipa;
+    let sql = (req.method === 'PUT') ? "UPDATE Jogador SET name = ?, birthdate = ?, nTelemovel=? , idEquipa = ? WHERE id = ?" : "INSERT INTO Jogador(name, birthdate, nTelemovel, idEquipa) VALUES (?,?,?,?)";
     connection.connect(function (err) {
         if (err) throw err;
-        connection.query(sql, [name, birthday, idCountry, req.params.id], function (err, rows) {
+        connection.query(sql, [name, birthday, nTelemovel, idEquipa, req.params.id], function (err, rows) {
             if (err) {
                 res.sendStatus(500);
             } else {
@@ -121,8 +174,76 @@ function createUpdatePerson(req, res) {
     });
 }
 
-function removePerson(req, res) {
-    let query = 'DELETE FROM person WHERE id = ?';
+function createUpdateTorneio(req, res){
+    let connection = mysql.createConnection(options);
+    let name = req.body.name;
+    let modalidade = req.body.modalidade;
+    let torneio = req.body.tipoTorneio;
+    let sql = (req.method === 'PUT') ? "UPDATE Torneio SET name = ?, modalidade = ?, tipoTorneio=? WHERE id = ?" : "INSERT INTO Torneio(name, modalidade, tipoTorneio) VALUES (?,?,?)";
+    connection.connect(function (err) {
+        if (err) throw err;
+        connection.query(sql, [name, modalidade, torneio, req.params.id], function (err, rows) {
+            if (err) {
+                res.sendStatus(500);
+            } else {
+                res.send(rows);
+            }
+        });
+    });
+}
+module.exports.createUpdateTorneio=createUpdateTorneio;
+
+function deleteTorneio(req, res){
+    let query = 'DELETE FROM torneio WHERE id = ?';
+    let connection = mysql.createConnection(options);
+    connection.connect(function (err) {
+        if (err) throw err;
+        connection.query(query, [req.params.id], function (err) {
+            if (err) {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(200);
+            }
+        });
+    });
+}
+module.exports.deleteTorneio=deleteTorneio;
+
+function createUpdateEquipa(req, res){
+    let connection = mysql.createConnection(options);
+    let name = req.body.name;
+    let sql = (req.method === 'PUT') ? "UPDATE equipa SET name = ? WHERE id = ?" : "INSERT INTO Equipa(name) VALUES (?)";
+    connection.connect(function (err) {
+        if (err) throw err;
+        connection.query(sql, [name, req.params.id], function (err, rows) {
+            if (err) {
+                res.sendStatus(500);
+            } else {
+                res.send(rows);
+            }
+        });
+    });
+}
+module.exports.createUpdateEquipa=createUpdateEquipa;
+
+function deleteEquipa(req, res){
+    let query = 'DELETE FROM equipa WHERE id = ?';
+    let connection = mysql.createConnection(options);
+    connection.connect(function (err) {
+        if (err) throw err;
+        connection.query(query, [req.params.id], function (err) {
+            if (err) {
+                res.sendStatus(404);
+            } else {
+                res.sendStatus(200);
+            }
+        });
+    });
+}
+module.exports.deleteEquipa=deleteEquipa;
+
+function removeJogador(req, res){
+    let query = 'DELETE FROM jogador WHERE id = ?';
     let connection = mysql.createConnection(options);
     connection.connect(function (err) {
         if (err) throw err;
@@ -136,42 +257,15 @@ function removePerson(req, res) {
     });
 }
 
-function removeJogador(req, res){
-    // todo
-}
-
-function removeTorneio(req, res){
-
-}
 
 
-
-/**
- * Função para retornar a lista de paises da BD.
- * @param {*} req 
- * @param {*} res 
- */
-function getCountries(req, res) {
-    let connection = mysql.createConnection(options);
-    connection.connect();
-    let query = "SELECT id, name, shortName FROM Country";
-    connection.query(query, function (err, rows) {
-        if (err) {
-            res.json({ "message": "Error", "error": err });
-        } else {
-            res.json({ "message": "Success", "country": rows });
-        }
-    });
-}
-/*
-module.exports.getPeople = getPeople;
-module.exports.removePerson = removePerson;
-module.exports.createUpdatePerson = createUpdatePerson;
-module.exports.getCountries = getCountries;
-*/
 module.exports.getJogador = getJogador;
 module.exports.getTorneio = getTorneio;
 module.exports.getEquipa = getEquipa;
 module.exports.getModalidade = getModalidade;
 module.exports.getTipoTorneio = getTipoTorneio;
 module.exports.getJogos = getJogos
+module.exports.getJogosFromTorneio = getJogosFromTorneio;
+module.exports.getEquipaFromTorneio=getEquipaFromTorneio;
+module.exports.createUpdateJogador = createUpdateJogador;
+module.exports.removeJogador=removeJogador;
